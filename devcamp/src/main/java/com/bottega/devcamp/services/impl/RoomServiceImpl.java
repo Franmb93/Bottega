6661aa393481febf721d7dd4418e36efbbfd1ede
franmb93 <fcomunozbetanzos@gmail.com>
2023-03-02 18:36:33 +0100

    [Back] Fixed typo in RoomServiceImpl

--- devcamp/src/main/java/com/bottega/devcamp/services/impl/RoomServiceImpl.java
+++ devcamp/src/main/java/com/bottega/devcamp/services/impl/RoomServiceImpl.java
@@ -7,13 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bottega.devcamp.entities.Room;
+import com.bottega.devcamp.repository.IRoomDao;
 import com.bottega.devcamp.services.IRoomService;
 
 @Service
 public class RoomServiceImpl implements IRoomService{
     
     @Autowired
-    private IRoomService dao;
+    private IRoomDao dao;
 
     @Override
     public List<Room> findAll() {
@@ -22,7 +23,7 @@ public class RoomServiceImpl implements IRoomService{
 
     @Override
     public Room findById(UUID id) {
-        return dao.findById(id);
+        return dao.findById(id).orElse(null);
     }
 
     @Override
@@ -32,7 +33,7 @@ public class RoomServiceImpl implements IRoomService{
 
     @Override
     public void delete(UUID id) {
-        dao.delete(id);
+        dao.deleteById(id);
     }
 
 
